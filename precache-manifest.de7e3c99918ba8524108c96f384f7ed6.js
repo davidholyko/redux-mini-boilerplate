d1657003163dca92451facf061e1c4b67e6764d7
David Ko <david.holy.ko@gmail.com>
2020-01-27 23:26:12 -0500

    Updates

---
+++ precache-manifest.de7e3c99918ba8524108c96f384f7ed6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8008c19115ad55bac78d97e49548f4ed",
+    "url": "/redux-mini-boilerplate/index.html"
+  },
+  {
+    "revision": "f2f5a8abe6c9a2af8a19",
+    "url": "/redux-mini-boilerplate/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a756a7d9515739b626c1",
+    "url": "/redux-mini-boilerplate/static/css/main.49053759.chunk.css"
+  },
+  {
+    "revision": "f2f5a8abe6c9a2af8a19",
+    "url": "/redux-mini-boilerplate/static/js/2.193fddc4.chunk.js"
+  },
+  {
+    "revision": "adf988142ef3edd1ada16f48aab2de8c",
+    "url": "/redux-mini-boilerplate/static/js/2.193fddc4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a756a7d9515739b626c1",
+    "url": "/redux-mini-boilerplate/static/js/main.901e2bb4.chunk.js"
+  },
+  {
+    "revision": "74c3bbc7bbfa853e8cef",
+    "url": "/redux-mini-boilerplate/static/js/runtime-main.15c70ef8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux-mini-boilerplate/static/media/logo.5d5d9eef.svg"
+  }
+]);
